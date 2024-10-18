fcb31e138d356215eca9a10095b4743b96485d63
Gintautas Kazlauskas <g@veil.gg>
2024-10-18 09:57:08 +0300

    Sober refactoring

--- addons/laigter/laigter.gd
+++ addons/laigter/laigter.gd
@@ -1,50 +1,59 @@
-extends EditorProperty
-
-# The main control for editing the property.
-var property_control = Button.new()
-var parent
-var texture_type
-
-func _init(object, type):
-	parent = object
-	texture_type = type
-	property_control.tooltip_text
-
-	add_child(property_control)
-	add_focusable(property_control)
-	property_control.pressed.connect(_on_button_pressed)
-
-func _on_button_pressed():
-	if texture_type == "normal_texture":
-		generate("-n")
-	elif texture_type == "specular_texture":
-		generate("-c")
+class LaigterPlugin extends EditorInspectorPlugin:
+    func _can_handle(object):
+        return object is CanvasTexture
 
 
-func generate(type):
-	var laigter_path = ProjectSettings.get_setting("filesystem/import/laigter/laigter_path")
-	var diffuse_texture = parent.diffuse_texture.resource_path.replace("res://", ProjectSettings.globalize_path("res://"))
-	var diff_file = parent.diffuse_texture.resource_path.get_file()
+    func _parse_property(object, type, name, hint_type, hint_string, usage_flags, wide):
+        if name in ["normal_texture", "specular_texture"]:
+            add_property_editor("laigter", LaigterEditor.new(object, name), true, "Laigter")
+        return false
 
-	var extension = "_n" if type == "-n" else "_s"
 
-	var gen_file = parent.diffuse_texture.resource_path.get_file().split(".")
-	gen_file[0] += extension
-	gen_file = ".".join(gen_file)
+class LaigterEditor extends EditorProperty:
+    var property_control = Button.new()
+    var laigter_path = ProjectSettings.get_setting("filesystem/import/laigter/laigter_path")
+    var parent
+    var texture_type
 
-	var gen_path = parent.diffuse_texture.resource_path.replace(diff_file, gen_file)
+    func _init(object, type):
+        parent = object
+        texture_type = type
+        add_child(property_control)
+        add_focusable(property_control)
+        property_control.pressed.connect(_on_button_pressed)
+        property_control.text = "Generate"
 
-	var output = []
-	var exit_code = OS.execute(laigter_path, ["-g", "-d", diffuse_texture, type], output, true, false)
-	if exit_code != 0:
-		push_error(output)
-		return
+    func _on_button_pressed():
+        generate()
 
-	var image = Image.load_from_file(gen_path)
-	var t = ImageTexture.create_from_image(image)
+    func generate():
+        var diffuse_texture = parent.diffuse_texture.resource_path.replace("res://", ProjectSettings.globalize_path("res://"))
+        var diff_file = parent.diffuse_texture.resource_path.get_file()
+        var extension
+        var type_arg
 
-	if type == "-n":
-		parent.normal_texture = t
-	elif type == "-c":
-		parent.specular_texture = t
-	print("Generated texture")
+        if texture_type == "normal_texture":
+            extension = "_n"
+            type_arg =  "-n"
+        elif texture_type == "specular_texture":
+            extension = "_s"
+            type_arg =  "-c"
+
+        var gen_file = diff_file.get_basename() + extension + "." + diff_file.get_extension()
+        var gen_path = parent.diffuse_texture.resource_path.replace(diff_file, gen_file)
+
+        var output = []
+        var exit_code = OS.execute(laigter_path, ["-g", "-d", diffuse_texture, type_arg], output, true, false)
+
+        if exit_code != 0:
+            push_error("Error generating texture: " + output.join("\n"))
+            return
+
+        var image = Image.load_from_file(gen_path)
+        var texture = ImageTexture.create_from_image(image)
+
+        if texture_type == "normal_texture":
+            parent.normal_texture = texture
+        elif texture_type == "specular_texture":
+            parent.specular_texture = texture
+        print("Generated " + " ".join(texture_type.split("_")))
--- addons/laigter/plugin.gd
+++ addons/laigter/plugin.gd
@@ -5,37 +5,46 @@ var plugin
 var laigter_path = ProjectSettings.get_setting("filesystem/import/laigter/laigter_path")
 
 func _enter_tree():
-	# Register settings for plugin
-	if ProjectSettings.get_setting("filesystem/import/laigter/laigter_path") == null:
-		ProjectSettings.set_setting("filesystem/import/laigter/laigter_path", "laigter")
-	ProjectSettings.set_initial_value("filesystem/import/laigter/laigter_path", "laigter")
-	ProjectSettings.add_property_info({
-		"name": "filesystem/import/laigter/laigter_path",
-		"type": TYPE_STRING,
-		"hint": PROPERTY_HINT_GLOBAL_FILE})
-	ProjectSettings.set_as_basic("filesystem/import/laigter/laigter_path", true)
+    register_settings()
+    ProjectSettings.settings_changed.connect(verify_laigter_path)
+    verify_laigter_path()
 
-	ProjectSettings.settings_changed.connect(verify_laigter_path)
-
-	verify_laigter_path()
-
-	plugin = preload("res://addons/laigter/inspector_plugin.gd").new()
-	add_inspector_plugin(plugin)
+    plugin = preload("res://addons/laigter/laigter.gd")
+    add_inspector_plugin(plugin.LaigterPlugin.new())
 
 
 func _exit_tree():
-	remove_inspector_plugin(plugin)
+    remove_inspector_plugin(plugin)
+
+
+func register_settings():
+    if ProjectSettings.get_setting("filesystem/import/laigter/laigter_path") == null:
+        ProjectSettings.set_setting("filesystem/import/laigter/laigter_path", "laigter")
+
+    ProjectSettings.set_initial_value("filesystem/import/laigter/laigter_path", "laigter")
+    ProjectSettings.add_property_info({
+        "name": "filesystem/import/laigter/laigter_path",
+        "type": TYPE_STRING,
+        "hint": PROPERTY_HINT_GLOBAL_FILE
+    })
+    ProjectSettings.set_as_basic("filesystem/import/laigter/laigter_path", true)
 
 
 func verify_laigter_path():
-	if laigter_path == ProjectSettings.get_setting("filesystem/import/laigter/laigter_path"):
-		return
-	else: laigter_path = ProjectSettings.get_setting("filesystem/import/laigter/laigter_path")
+    var current_path = ProjectSettings.get_setting("filesystem/import/laigter/laigter_path")
 
-	# Check if Laigter binary is available
-	var output = []
-	var exit_code = OS.execute(laigter_path, ["-h"], output, true, false)
+    if laigter_path == current_path:
+        return
 
-	if exit_code != 0:
-		push_error("Failed to verify Laigter binary, set the path to the Laigter binary in Project Settings -> Input:")
-		for line in output: push_error(line)
+    laigter_path = current_path
+    check_laigter_binary()
+
+
+func check_laigter_binary():
+    var output = []
+    var exit_code = OS.execute(laigter_path, ["-h"], output, true, false)
+
+    if exit_code != 0:
+        push_error("Failed to verify Laigter binary. Set the path to the Laigter binary in Project Settings -> Input:")
+        for line in output:
+            push_error(line)
